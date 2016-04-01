5fd9cceb896e5ad6781374f0a949475a7947a1c9
Emily Chiu <echiu@vmwhnsqa01.gtn.hnops.net>
2016-04-01 19:18:16 -0400

    mobius_strip + parallel_tree

--- lesson3/demo/wiki.py
+++ lesson3/demo/wiki.py
@@ -5,7 +5,7 @@ from time import *
 
 bgcolor("black")
 
-def serial(n, size):
+def wiki_serial(n, size):
   fd(100)
   pencolor("red")
   pensize(3)
@@ -15,7 +15,10 @@ def serial(n, size):
      circle(size)
   update()
 
-def parallel(n,size,speed):
+def wiki_serialEvent(x,y):
+   wiki_serial(36,100)
+
+def wiki_parallel(n,size,speed,speedup,slowdown):
     def create_turtles(n):
       for i in range(n):
         t=Turtle()
@@ -24,6 +27,7 @@ def parallel(n,size,speed):
         #t.pencolor("white")
         t.seth(360/n*i)
     clearscreen()
+    tracer(speedup,slowdown)
     create_turtles(n)
     s=turtles()
     steps = 20
@@ -32,14 +36,28 @@ def parallel(n,size,speed):
       for i in range(n):
          s[i].fd(step)
          s[i].rt(360./steps)
+    update()  #Comment to skip circles
+ 
        
-def timedraw():
-    for i in range(11):
-      tp0= clock()
-      tw0= time()
-      parallel(36,100,i)
-      print("when speed is ",i,"Process time is ",clock()-tp0," Wall time is ",time()-tw0 )
+def timedraw(speedrange,speeduprange,slowdown):
+    for i in speeduprange:
+      for j in speedrange:
+        tp0= clock()
+        tw0= time()
+        wiki_parallel(36,100,j,i,slowdown)
+        print("when speedup is ",j,"when speed is ",i,"Process time is ",clock()-tp0," Wall time is ",time()-tw0 )
 
-timedraw()   
-onclick(serial(36,100))
+#wiki_parallel(36,100,0) # speed 1 is slowest tracer speedup=1, 270s at slowest=1, 30s at fastest=0
+timedraw([1],[1],0) # tracer 2 is slowest tracer speedup=(0, 0.15s), (1,11s),(2,32s),(4,16s),(64,1s), slwodown has no effect
+onscreenclick(wiki_serialEvent)
 done()
+
+#Macrocontrol speedup=0  for very fast, speedup=2 for verys low.  speedup=1 and fine tune speed with 8x between 0 and 1
+#generally (0,x) fastest, (1,1) slowest, but make sure to check (2,1) which is sometimes slower.
+#when speed is  0 speedup is  1 Process time is  2.6399999999999997  Wall time is  11.750622749328613
+#when speed is  1 speedup is  1 Process time is  25.490000000000002  Wall time is  106.69523286819458
+#when speed is  0 speedup is  2 Process time is  24.43  Wall time is  34.93558883666992
+#when speed is  1 speedup is  2 Process time is  24.499999999999996  Wall time is  35.08503341674805
+#Surprise! Notice speedup 2 is actually slower than speedup 1 for certain speeds
+
+
