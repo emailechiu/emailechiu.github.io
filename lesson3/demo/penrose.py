596a8cc7dccc7b4307c819f5d40917a5826c7e1f
echiu <emailechiu@gmail.com>
2016-04-07 00:52:13 -0400

    penrose

--- lesson3/demo/penrose.py
+++ lesson3/demo/penrose.py
@@ -4,9 +4,38 @@ mode("standard")
 radius=100
 angle=72
 sides=25
-begin_poly()
-goto(radius*cos(radians(-angle)/2),radius*sin(-radians(angle)/2))
-seth(angle/2+360/sides)
-circle(radius,angle,sides)
-home()
+def dart(d):
+    fd(d)
+    rt(36)
+    fd(d)
+    lt(144)
+    fd(d)
+    lt(108)
+    fd(d)
+    lt(144) 
+
+def kite(d):
+   def kite(l):
+    fl = f * l
+    lt(36)
+    fd(l)
+    rt(108)
+    fd(fl)
+    rt(36)
+    fd(fl)
+    rt(108)
+    fd(l)
+    rt(144)
+
+def dart(l):
+    fl = f * l
+    lt(36)
+    fd(l)
+    rt(144)
+    fd(fl)
+    lt(36)
+
+
+
+
 
