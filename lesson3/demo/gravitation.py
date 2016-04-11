ad99d96554f12f2ed2d0245ece823b1b6e887e04
echiu <emily.chiu@hughes.com>
2016-04-11 19:29:37 -0400

    gravitation

--- lesson3/demo/gravitation.py
+++ lesson3/demo/gravitation.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 from turtle import *
-setworldcoordinates(-1,-1,1,1)
+from math import *
+AU=3e8*60*8.31
+TU=365*24*3600
+TU=1
+GMs=pow(AU,3)*pow((2*pi/TU),2)
+GMe=pow(AU/363,3)*pow(2*pi/TU*12,2)
+print(GMs)
+setworldcoordinates(-AU,-AU,AU,AU)
 s=Turtle(shape='circle');s.color('yellow')
 e=Turtle(shape='circle');e.color('blue')
 m=Turtle(shape='circle');m.color('purple')
-F=k*abs(pos)
-e.goto(0,-0.9)
-e.circle(0.9)
+e.goto(0,-0.99*AU)
+e.circle(0.99*AU)
+Oe = 2*pi/TU
+Oem = 2*pi/TU*12
+
+def move():
+    Ve=Ve + G*Ms/D(e,s)
+    Vem=Vem+G*Me/D(e,m)
 
 
