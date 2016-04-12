4c685e3f20ab170a335d1729056a246c14f5d8e5
echiu <emailechiu@gmail.com>
2016-04-12 01:04:34 -0400

    gravitation

--- lesson3/demo/gravitation.py
+++ lesson3/demo/gravitation.py
@@ -4,20 +4,29 @@ from math import *
 AU=3e8*60*8.31
 TU=365*24*3600
 TU=1
-GMs=pow(AU,3)*pow((2*pi/TU),2)
-GMe=pow(AU/363,3)*pow(2*pi/TU*12,2)
-print(GMs)
+Oe = 2*pi/TU
+Oem = 2*pi/TU*13
+Ve = Oe * Re
+V
+GMs=pow(AU,3)*pow(Oe,2)
+GMe=pow(AU/388,3)*pow(Oem,2)
+Rm=1.28*3e8
+Re=AU
+print(GMs/GMe)
 setworldcoordinates(-AU,-AU,AU,AU)
 s=Turtle(shape='circle');s.color('yellow')
 e=Turtle(shape='circle');e.color('blue')
 m=Turtle(shape='circle');m.color('purple')
-e.goto(0,-0.99*AU)
-e.circle(0.99*AU)
-Oe = 2*pi/TU
-Oem = 2*pi/TU*12
-
+e.goto(0,-Re)
+#e.circle(Re)
+m.goto(0,-Re-Rm)
 def move():
-    Ve=Ve + G*Ms/D(e,s)
+    rms = s.pos()-m.pos()
+    rme = e.pos()-m.pos()
+    res = s.pos()-e.pos()
+    Ae = GMs/pow(abs(res),3)*res 
+    Am = GMs/pow(abs(rms),3)*rms+GMe/pow(abs(rme),3)*rme
+    Ve=Ve + 
     Vem=Vem+G*Me/D(e,m)
 
 
