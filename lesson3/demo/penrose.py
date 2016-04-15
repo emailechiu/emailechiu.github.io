7efab1ce06b989a92e078f45b2859e5bd009797e
echiu <emailechiu@gmail.com>
2016-04-15 00:47:45 -0400

    penrose

--- lesson3/demo/penrose.py
+++ lesson3/demo/penrose.py
@@ -1,6 +1,7 @@
 from turtle import *
 from math import *
-d=400
+from time import sleep
+d=50
 r=(sqrt(5)-1)/2
 s = r*d
 def dart():
@@ -37,37 +38,49 @@ def kite():
     end_fill()
     return get_poly()
 
+def tiledart(depth):
+    if depth==0:
+          shape('dart')
+          shapesize(1)
+          color('black','yellow')
+          stamp()
+    else:
+          tiledart(depth-1)
+          lt(36);fd(d*r**(-depth));rt(144);tilekite(depth-1);lt(144);bk(d*r**(-depth))
+          rt(36);fd(d*r**(-depth));lt(144);tilekite(depth-1);rt(144);bk(d*r**(-depth))
+
 def tilekite(depth):
-    print(depth)
-    if depth>=2: 
+    #print(depth)
+    if depth==0: 
           shape('kite')
-          shapesize(r**depth)
+          shapesize(1)
           color('black','blue')
           stamp()
     else: 
-          shape('dart')
-          color('black','yellow')
-          shapesize(r**(depth+1))
-          stamp()
-          fd(d*r**(depth))
+          tiledart(depth-1)
+          fd(d*r**(-depth))
           rt(180)
-          lt(36);tilekite(depth+1);rt(36)
-          rt(36);tilekite(depth+1);lt(36)
+          lt(36);tilekite(depth-1);rt(36)
+          rt(36);tilekite(depth-1);lt(36)
           lt(180)
-          bk(d*r**(depth))
+          bk(d*r**(-depth))
 
 def tile():
-    for i in range(1):
-      tilekite(0)
+   ht()
+   for j in range(3): 
+    reset()
+    ht()
+    for i in range(5):
+      tilekite(j)
       lt(72)
+    update()
+    sleep(1)
       
  
-tracer(2)
+tracer(0)
 register_shape('dart',dart())
 register_shape('kite',kite())
-reset()
 tile()
-update()
 exitonclick()
 
 
