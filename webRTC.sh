05412d920c72c7d2193b8363cfeb750df71975e0
echiu <emailechiu@gmail.com>
2016-09-04 01:22:14 -0400

    ¨webRTC¨

---
+++ webRTC.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get distupgrade
+sudo apt-get dist-upgrade
+update-nodejs-and-nodered 
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+npm install websocket
+git clone git://github.com/buildar/getting_started_with_webrtc
+cd getting_started_with_webrtc
+#nano video_call_with_chat_and_file_sharing.html
+node webrtc_signal_server.js
+
