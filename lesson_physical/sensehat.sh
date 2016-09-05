5f2723fe01dc58c13dba1cb12a4e52c91bd5919d
echiu <emailechiu@gmail.com>
2016-09-05 17:21:21 -0400

    sensehat

---
+++ lesson_physical/sensehat.sh
@@ -0,0 +1,3 @@
+npm install node-red-node-pi-sense-hat-simulator
+sudo apt-get install sense-hat
+git clone https://github.com/TeCoEd/Rock-Paper-Scissors-Lizard-Spock-Sense-HAT-style_.git
---
+++ nodered.sh
@@ -0,0 +1,3 @@
+systemctl enable nodered.service
+cd ~/.nodered
+npm install node-red-node
