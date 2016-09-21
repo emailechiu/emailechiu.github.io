855c4d883e733e745323d800e8ab8c123cd0d180
echiu <emailechiu@gmail.com>
2016-09-20 22:13:24 -0400

    sensehat

--- lesson_physical/sensehat.sh
+++ lesson_physical/sensehat.sh
@@ -1,5 +1,4 @@
-wget -O - https://www.raspberrypi.org/files/astro-pi/astro-pi-install.sh --no-check-certificate | bash
-
+git clone https://github.com/RPi-Distro/python-sense-hat.git
 npm install node-red-node-pi-sense-hat-simulator
 sudo apt-get install sense-hat
 git clone https://github.com/TeCoEd/Rock-Paper-Scissors-Lizard-Spock-Sense-HAT-style_.git
