1d1e411ffb440e29030606564784b71445579ec1
root <root@vmwhnsqa01.gtn.hnops.net>
2016-09-27 18:59:08 -0400

    tictactoe assignment

--- lesson_physical/tictactoe.py
+++ lesson_physical/tictactoe.py
@@ -12,15 +12,15 @@ state=[0]*9
 who=1
 presses=0
 
-def add3(a,b):
+def add3(a,b): # given a =[1,2,3],b=[5,8,-2]  add(a,b)=>[6,10,1]
     return (abs(a[0]+b[0]),abs(a[1]+b[1]),abs(a[2]+b[2]))
     
-def clamp(value):
+def clamp(value): # clamp(3)=2 clamp(-1)=0 clamp(1)=1 clamp(2)=2 clamp(0)=0
     if value>2: value=2
     if value<0: value=0
     return value
 
-def board():
+def board(): # blue color for x=2 and y=0-7, x=5 and y=0-7, same with y
     color=(0,0,255)
     for i in range(0,8):
         sense.set_pixel(2,i,color)
@@ -28,7 +28,7 @@ def board():
         sense.set_pixel(5,i,color)
         sense.set_pixel(i,5,color)
 
-def display(pos,add):
+def display(pos,add): # display(0,0) will light up the first 4-LED square
     global who
     px=pos%3*3
     py=pos//3*3
@@ -39,7 +39,7 @@ def display(pos,add):
     for z in range(0,4):
         sense.set_pixel(px+z//2,py+z%2,newcolor)
 
-def pushed_up(event):
+def pushed_up(event): # move the display position up, shut off the previous display
     global y,x,who,entered
     if event.action!=stick.ACTION_RELEASED:
         #print("UP")
@@ -87,12 +87,8 @@ def pushed_enter(event):
                 sense.set_pixels(colors)
                 sleep(1)
                 restart()               
-##def pushed_any(event):
-##    global x,y
-##    if event.action!=stick.ACTION_RELEASED:
-##        print(x," x and y ",y)
 
-def evaluate():
+def evaluate(): # see if there is any winners
     global state
     for i in range(0,3):
         if state[i*3]+state[i*3+1]+state[i*3+2]==3: return 1
@@ -106,7 +102,7 @@ def evaluate():
     if sum(list(map(abs,state)))==9: return 0
     return 999
 
-def restart():
+def restart(): # if game ended restart it by resetting all the global variables
     global x,y,entered,state,who
     sense.clear()
     #sense.set_rotation(180)
@@ -117,13 +113,12 @@ def restart():
     who=1
     board()
 
-
+# Here is the main program
 mystick.direction_up=pushed_up
 mystick.direction_down=pushed_down
 mystick.direction_left=pushed_left
 mystick.direction_right=pushed_right
 mystick.direction_middle=pushed_enter
-#mystick.direction_any=pushed_any
 restart()
 while presses>=0:
     presses=-1
